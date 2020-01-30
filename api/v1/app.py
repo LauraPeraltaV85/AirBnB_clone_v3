4216ca611107fe7ec232dbd051eae6472abbc6e4
LauraPeraltaV85 <lalaperalta85@gmail.com>
2020-01-30 04:43:04 +0000

    [fix]

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,7 +5,7 @@ AirBnB App
 from flask import Flask, Blueprint, jsonify
 from models import storage
 from api.v1.views import app_views
-import os
+from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -27,7 +27,7 @@ def error_404(e):
     return(jsonify(error="Not found"), 404)
 
 if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST')
-    port = os.getenv('HBNB_API_POST')
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = os.getenv('HBNB_API_POST', 5000)
 
     app.run(host=host, port=port, threaded=True)
